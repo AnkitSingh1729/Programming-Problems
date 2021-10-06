f7b9c1d3a860b7ede8ba97968785183b45b143f4
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-10-06 23:14:34 +0530

    Single BST class

--- BST.js
+++ BST.js
@@ -1,31 +1,30 @@
-class Node {
-    constructor(val) {
+// This file contains a class for Binary Search Tree. We can create and object of BST and use the insert and search methords of BST
+
+class BST {
+    static root;
+    constructor (val){
         this.value = val;
         this.left = null;
         this.right = null;
     }
-}
-
-class BST {
-    constructor (val){
-        this.root = new Node(val);
-    }
+    // This function insert the input value in the bst with root-node = node
     insert = function (node, val) {
         if (node.value < val){
             if (node.right !== null){
                 this.insert(node.right, val);
             }else {
-                node.right = new Node(val);
+                node.right = new BST(val);
             }
         }else {
             if (node.left !== null){
                 this.insert(node.left, val);
             }else {
-                node.left = new Node(val);
+                node.left = new BST(val);
             }
         }
     }
 
+    // This function returns true if val is present in BST with root-node = node, else returns false
     search = function (node, val) {
         if (node.value === val){
             return true;
@@ -44,18 +43,24 @@ class BST {
 }
 
 
-currRoot = new BST(10);
-console.log(currRoot.root.value);
-currRoot.insert(currRoot.root, 12);
-currRoot.insert(currRoot.root, 22);
-currRoot.insert(currRoot.root, 2);
-currRoot.insert(currRoot.root, 132);
-currRoot.insert(currRoot.root, 152);
 
-console.log(currRoot.search(currRoot.root, 2));
-console.log(currRoot.search(currRoot.root, 152));
-console.log(currRoot.search(currRoot.root, 32));
-console.log(currRoot.search(currRoot.root, 22));
+// Initializing BST root
+root = new BST(10);
+console.log(root.value);
+
+// Inserting multiple elements in BST
+root.insert(root, 12);
+root.insert(root, 22);
+root.insert(root, 2);
+root.insert(root, 132);
+root.insert(root, 152);
+
+// Check if given number is present in BST or not
+console.log(root.search(root, 2));
+console.log(root.search(root, 152));
+console.log(root.search(root, 32));
+console.log(root.search(root, 22));
+console.log(root.search(root, 1));
 
 
 
